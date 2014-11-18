1c7a39e1b479ed5ee0be02a17e6e0c2bb5eff338
Tony DiPasquale <tony@thoughtbot.com>
2014-11-18 10:54:52 -0500

    Setup Dropbox SDK

---
+++ Frameworks/Dropbox.framework/Headers/DBDatastoreManager.h
@@ -0,0 +1,142 @@
+/* Copyright (c) 2013 Dropbox, Inc. All rights reserved. */
+
+#import "DBAccount.h"
+#import "DBAccountManager.h"
+#import "DBDatastore.h"
+#import "DBError.h"
+
+
+/** The datastore manager lets you list, create, open, and delete datastores. You can
+ also add an observer to find out when the list of datastores changes.
+*/
+@interface DBDatastoreManager : NSObject
+
+/** Gets the datastore manager for an account that has been linked via the account manager.
+
+ The returned object will be the only datastore manager for this account until you release
+ it, call <shutDown>, or the account is unlinked.  Calling this method again in the
+ mean time will return the same object. */
++ (DBDatastoreManager *)managerForAccount:(DBAccount *)account;
+
+/** Gets the local datastore manager for the accountManager. */
++ (DBDatastoreManager *)localManagerForAccountManager:(DBAccountManager *)accountManager;
+
+/** A convenient place to get your app's datastore manager. You must set it with `setSharedManager` first. */
++ (DBDatastoreManager *)sharedManager;
+
+/** Set your app's datastore manager using this method. Retrieve it any time with `sharedManager`. */
++ (void)setSharedManager:(DBDatastoreManager *)manager;
+
+/** Opens the default datastore for this account, or creates it if it doesn't exist.
+
+ @return The default datastore if successful, or `nil` if an error occurred. */
+- (DBDatastore *)openDefaultDatastore:(DBError **)error;
+
+/** Lists the <DBDatastoreInfo> for each of the user's datastores, including
+ the default datastore if it has been created.
+
+ @return A list of datastore <DBDatastoreInfo> objects if successful, or `nil` if an error occurred. */
+- (NSArray *)listDatastores:(DBError **)error;
+
+/** Gets a map of ID to the <DBDatastoreInfo> for each of the user's datastores, including the
+ default datastore if it has been created. This method returns the most recent information
+ from the server, but is overridden with the local version any time a local datastore has been changed
+ and `-[DBDatastore sync:]` has been called (i.e. the changes have not yet been uploaded to the server).
+
+ @return A map of ID to the datastore <DBDatastoreInfo> objects if successful, or `nil` if an error occurred. */
+- (NSDictionary *)listDatastoreInfo:(DBError **)error;
+
+/** Returns a new `DBDatastoreManager` created by migrating a local `DBDatastoreManager` to
+ the given account.
+
+ This will move all datastores and data from the local `DBDatastoreManager`
+ to the new `DBDatastoreManager`.  The new manager will immediately begin
+ uploading to the server, and merging with any existing changes on the server
+
+ The data is moved not copied, so the local datastore manager will be empty after migration.
+ Migration should be done to a freshly linked account which contains no unuploaded datastore
+ changes.  If that isn't the case, any datastore changes in the target account which
+ will be overwritten by the migrated data.
+
+ This must be called on a local `DBDatastoreManager`, and all of its datastores must
+ be closed. If the account provided ever had a `DbxDatastoreManager` it must be
+ shut down.  After this call, the current local `DbxDatastoreManager` will be shut
+ down and emptied.
+
+ @return The new datastore manager linked to the account, or `nil` if an error occurred. */
+- (DBDatastoreManager *)migrateToAccount:(DBAccount *)account error:(DBError **)error;
+
+/** Open an existing datastore by its ID.
+
+ The same datastore can't be opened more than once.
+
+ @return The datastore with the given ID if successful, or `nil` if an error occurred. */
+- (DBDatastore *)openDatastore:(NSString *)datastoreId error:(DBError **)error;
+
+/** Creates and opens a new datastore with a unique ID.
+
+ @return The newly created datastore, or `nil` if an error occcurred. */
+- (DBDatastore *)createDatastore:(DBError **)error;
+
+/** Opens the datastore with the given ID, creating it if it does not already exist.
+
+ Datastores can be created offline with this method, and their contents will be
+ merged with any datastore of the same name when the app is online again.
+
+ The same datastore can't be opened more than once.
+ 
+ Call `-[DBDatastore isValidId:]` to check input strings before using them as a
+ datastore ID.
+
+ @return The datastore with the given ID if successful, or `nil` if an error occurred. */
+- (DBDatastore *)openOrCreateDatastore:(NSString *)datastoreId error:(DBError **)error;
+
+/** Deletes a datastore with the given ID.
+
+ You must close open datastores before deleting them.
+
+ @return YES if the datastore was deleted, or NO if an error occurrred. */
+- (BOOL)deleteDatastore:(NSString *)datastoreId error:(DBError **)error;
+
+/** Removes a datastore from the local cache.
+
+ You must close open datastores before uncaching them.
+
+ Any changes not yet uploaded to the server are discarded on uncache. If the datastore
+ has such changes its <DBDatastoreStatus> has its `incoming` property set to `YES`.
+ The next time a datastore is opened its entire snapshot is downloaded from the server.
+
+ @return YES if the datastore was uncached, or NO if an error occurred. */
+- (BOOL)uncacheDatastore:(NSString *)datastoreId error:(DBError **)error;
+
+/** Add a block to be called when a datastore is added or removed.
+
+ Observers will always be called in the main thread. */
+- (void)addObserver:(id)obj block:(DBObserver)block;
+
+/** Remove all blocks associated with the given observer. */
+- (void)removeObserver:(id)obj;
+
+/** Shuts down the datastore manager, which stops all syncing.
+
+ All associated `DBDatastore`s will be closed.  Unsynced changes to unclosed
+ datastores will be lost. Changes that were synced before shutdown but not yet
+ uploaded will be uploaded the next time that particular datastore is opened.
+ 
+ After this call, the `DBDatastoreManager` and its `DBDatastore`s can no longer be used.
+ You should get a new `DBDatastoreManager` via <managerForAccount:>.
+
+ The datastore manager will be automatically shut down if the app is unlinked remotely. */
+- (void)shutDown;
+
+/** Whether the datastore manager is currently shut down. */
+@property (nonatomic, readonly, getter=isShutDown) BOOL shutDown;
+
+/** The [account](DBAccount) object this manager was created with. Will be `nil` if this
+ is the local manager. */
+@property (nonatomic, readonly) DBAccount *account;
+
+/** Whether this is the local manager. */
+@property (nonatomic, readonly) BOOL isLocal;
+
+@end
---
+++ Frameworks/Dropbox.framework/Headers/DBError.h
@@ -0,0 +1,94 @@
+/* Copyright (c) 2012 Dropbox, Inc. All rights reserved. */
+
+/** Value set for the `domain` property of a <DBError>. */
+extern NSString * const DBErrorDomain;
+
+/** Value set for the `name` property of a <DBException>. */
+extern NSString * const DBExceptionName;
+
+#ifndef NS_ENUM
+#define NS_ENUM(_type, _name) _type _name; enum
+#endif
+
+/** Error codes for the `DBErrorDomain` domain.  The numeric values of these
+ codes are subject to change between releases, and shouldn't be relied on. */
+typedef NS_ENUM(NSInteger, DBErrorCode) {
+    DBErrorUnknown = 0,
+
+    // These errors are usually considered fatal, raising exceptions,
+    // rather than returning a DBError.  The codes may be used for
+    // background sync status.
+    DBErrorInternal             = -1000,
+    DBErrorCache                = -1001,
+    DBErrorShutdown             = -1002,
+    DBErrorClosed               = -1003,
+    DBErrorDeleted              = -1004,
+    DBErrorBadType              = -1007,
+    DBErrorSizeLimit            = -1008,
+    DBErrorBadIndex             = -1009,
+    DBErrorIllegalArgument      = -1010,
+    DBErrorBadState             = -1011,
+    DBErrorMemory               = -1900,
+    DBErrorSystem               = -1901,
+    DBErrorNotCached            = -2000,
+
+    // These errors indicate local errors with the API.
+    DBErrorInvalidOperation     = -10000,
+    DBErrorNotFound             = -10001,
+    DBErrorExists               = -10002,
+    DBErrorAlreadyOpen          = -10003,
+    DBErrorParent               = -10004,
+    DBErrorDiskSpace            = -10006,
+    DBErrorDisallowed           = -10007,
+    DBErrorFileIO               = -10008,
+    DBErrorCancelled            = -10009,
+    DBErrorReadOnly             = -10010,
+
+    // Errors with network communication.
+    DBErrorNetwork              = -11000,
+    DBErrorTimeout              = -11001,
+    DBErrorConnection           = -11002,
+    DBErrorSsl                  = -11003,
+
+    // Errors during the processing of a server request.
+    DBErrorServer               = -11004,
+    DBErrorAuth                 = -11005,
+    DBErrorQuota                = -11006,
+    DBErrorRequest              = -11008,
+    DBErrorResponse             = -11009,
+    DBErrorRetryLater           = -11010,
+
+    // Errors specific to DBFilesystem functionality.
+    DBErrorParamsNoThumb        = -12000,
+};
+
+
+/** The `DBError` class is a subclass of `NSError` that always has `domain` set to `DBErrorDomain`.
+ <!-- paragraph separator in class docs for appledoc bug -->
+ Any method expected to fail will return a `DBError` object via the last parameter.
+ Additionally, errors that happen in the background via syncing can also be retrieved, such
+ as the error property on <DBFileStatus>.
+ <!-- paragraph separator in class docs for appledoc bug -->
+ Some failures (those which represent bugs or internal errors) will instead raise a <DBException>
+ when they occur in an API method. A `DBError` will still be used if such a failure occurs
+ on a background thread. */
+@interface DBError : NSError
+
+/** Same as `code`. The code on a DBError object is always listed in the DBErrorCode enum. */
+- (DBErrorCode)dbErrorCode;
+
+@end
+
+
+/** The `DBException` class is a subclass of `NSException` that always has `name` set to
+ `DBExceptionName`. A `DBException` is raised by a failure in an API method which indicates
+ programming errors or internal SDK problems.
+ <!-- paragraph separator in class docs for appledoc bug -->
+ You should generally not have to catch a `DBException`, but the `error` property will allow you
+ to classify or translate one if you need to. */
+@interface DBException : NSException
+
+/** Information about the error which caused this exception to be raised. */
+@property (nonatomic, readonly) DBError *error;
+
+@end
