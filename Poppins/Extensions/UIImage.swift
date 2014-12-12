12b4c3a2d46bbd29a5b91679cc914bb6d746b071
Tony DiPasquale <tony@thoughtbot.com>
2014-12-12 16:50:08 -0500

    Use Gifu for animating gifs

--- Poppins/Extensions/UIImage.swift
+++ Poppins/Extensions/UIImage.swift
@@ -1,5 +1,8 @@
-extension UIImage {
-    class func sizeForImageData(data: NSData) -> CGSize? {
-        return UIImage(data: data)?.size
-    }
+func imageForData(data: NSData) -> UIImage? {
+    return UIImage(data: data)
+}
+
+func imageSizeConstrainedByWidth(width: CGFloat)(image: UIImage) -> CGSize {
+    let aspectRatio = image.size.height / image.size.width
+    return CGSize(width: width, height: width * aspectRatio)
 }
--- Poppins/Views/PoppinsCell.swift
+++ Poppins/Views/PoppinsCell.swift
@@ -1,3 +1,12 @@
+import Gifu
+
 class PoppinsCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
+
+    func configureWithImageData(data: NSData) {
+        let size = UIImage(data: data) >>- imageSizeConstrainedByWidth(frame.width) ?? CGSizeZero
+        let image = AnimatedImage(data: data, size: size)
+        imageView.setAnimatedImage(image)
+        imageView.startAnimatingGIF()
+    }
 }
