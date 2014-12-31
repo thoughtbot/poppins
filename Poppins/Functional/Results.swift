195b0cffd29ae3ed338ba2e9cd687946eb9e7957
Gordon Fontenot <gordon@thoughtbot.com>
2014-12-31 09:56:49 -0500

    Use LlamaKit's map and flatMap for fmap and bind
    We don't need to duplicate these implementations, since they are already
    supplied by LlamaKit. Instead, we should just use them for the
    implementation of our operators.

--- Poppins/Functional/Results.swift
+++ Poppins/Functional/Results.swift
@@ -1,10 +1,11 @@
 import LlamaKit
 
 func >>-<A, B>(a: Result<A>, f: A -> Result<B>) -> Result<B> {
-    switch a {
-    case let .Success(box): return f(box.unbox)
-    case let .Failure(err): return failure(err)
-    }
+    return a.flatMap(f)
+}
+
+func <^><A, B>(f: A -> B, a: Result<A>) -> Result<B> {
+    return a.map(f)
 }
 
 func <*><A, B>(f: Result<A -> B>, a: Result<A>) -> Result<B> {
@@ -13,12 +14,5 @@ func <*><A, B>(f: Result<A -> B>, a: Result<A>) -> Result<B> {
     case let (.Failure(err), _): return failure(err)
     case let (_, .Failure(err)): return failure(err)
     default: return failure(NSError())
-    }
-}
-
-func <^><A, B>(f: A -> B, a: Result<A>) -> Result<B> {
-    switch a {
-    case let .Success(box): return success(f(box.unbox))
-    case let .Failure(err): return failure(err)
     }
 }
