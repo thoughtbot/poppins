c94a8a284555bc8bde62b3735ffd00ba23657644
Gordon Fontenot <gordon@thoughtbot.com>
2014-12-31 10:05:10 -0500

    Use the built in `map` for Optional as fmap
    Along the same lines as 765fdd2, we shouldn't need to duplicate this
    implementation. Instead, we can use the implementation provided by the
    standard lib.

--- Poppins/Functional/Optional.swift
+++ Poppins/Functional/Optional.swift
@@ -8,10 +8,7 @@ func >>-<A, B>(a: A?, f: A -> B?) -> B? {
 }
 
 func <^><A, B>(f: A -> B, a: A?) -> B? {
-    switch a {
-    case let .Some(x): return f(x)
-    case .None: return .None
-    }
+    return a.map(f)
 }
 
 func <*><A, B>(f: (A -> B)?, a: A?) -> B? {
