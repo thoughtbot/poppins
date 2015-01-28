3751c400d27fba18aeb2443325e78f8774cad59d
Tony DiPasquale <tony@thoughtbot.com>
2015-01-28 14:12:58 -0500

    Abstract ImageFetcher from cell controller

--- Poppins/Controllers/PoppinsCellController.swift
+++ Poppins/Controllers/PoppinsCellController.swift
@@ -1,15 +1,10 @@
 import Gifu
 import Runes
 
-private let ImageCache = Cache<[AnimatedFrame]>()
-private let purger = CachePurger(cache: ImageCache)
-private let operationQueue = AsyncQueue(name: "PoppinsCacheQueue", maxOperations: 10)
-
 class PoppinsCellController {
-    let manager: SyncManager
+    let imageFetcher: ImageFetcher
     let path: String
     var observer: ViewModelObserver?
-    private var operation: NSBlockOperation?
 
     var viewModel: PoppinsCellViewModel {
         didSet {
@@ -17,39 +12,15 @@ class PoppinsCellController {
         }
     }
 
-    init(manager: SyncManager, path: String) {
-        self.manager = manager
+    init(imageFetcher: ImageFetcher, path: String) {
+        self.imageFetcher = imageFetcher
         self.path = path
         viewModel = PoppinsCellViewModel(frames: [])
     }
 
-    deinit {
-        operation?.cancel()
-    }
-
     func fetchImage(size: CGSize) {
-        operation = NSBlockOperation(block: { [unowned self] in
-            _ = self.getImage(size)
-        })
-        operationQueue.addOperation <^> operation
-    }
-
-    func getImage(size: CGSize) {
-        purger.thumbsUpGoodJob()
-        var image = ImageCache.itemForKey(path)
-
-        if image == nil {
-            if operation?.cancelled ?? true { return }
-            image = curry(AnimatedFrame.createWithData)
-                <^> manager.getFile(path).value
-                <*> size
-
-            curry(ImageCache.setItem) <^> image <*> path
-        }
-
-        if let x = image {
-            if operation?.cancelled ?? true { return }
-            viewModel = PoppinsCellViewModel(frames: x)
+        imageFetcher.fetchImage(size, path: path) {
+            self.viewModel = PoppinsCellViewModel(frames: $0)
         }
     }
 }
