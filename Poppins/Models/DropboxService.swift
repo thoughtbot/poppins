963cabf11961c6091f19d8c4910cbaecd3345605
Tony DiPasquale <tony@thoughtbot.com>
2015-01-29 14:17:08 -0500

    Use Dropbox Core API for authentication
    This switches to the Core API for authentication. It abstracts the
    linking of an external service from the former SyncManager in
    preperation for building our own sync engine.

--- Poppins/Models/DropboxService.swift
+++ Poppins/Models/DropboxService.swift
@@ -1,80 +1,27 @@
-import LlamaKit
 import Runes
 
-class DropboxService : SyncableService {
+class DropboxService : LinkableService {
     let type: Service = .Dropbox
-    var observer: ServiceUpdateObserver? = .None
 
     func setup() {
-        let manager = DBAccountManager(appKey: "j77mzt1vvjloikh", secret: "y3rw9dlmd72dkr3")
-        DBAccountManager.setSharedManager(manager)
-
-        if isLinked() {
-            setupFilesystem <^> DBAccountManager.sharedManager().linkedAccount
-        }
+        let session = DBSession(appKey: "j77mzt1vvjloikh", secret: "y3rw9dlmd72dkr3", root:kDBRootAppFolder)
+        DBSession.setSharedSession(session)
     }
 
     func initiateAuthentication<T>(controller: T) {
-        DBAccountManager.sharedManager().linkFromController <^> controller as? UIViewController
+        DBSession.sharedSession().linkFromController <^> controller as? UIViewController
     }
 
     func finalizeAuthentication(url: NSURL) -> Bool {
-        let account = DBAccountManager.sharedManager().handleOpenURL(url)
-        setupFilesystem <^> account
+        let account = DBSession.sharedSession().handleOpenURL(url)
         return account != .None
     }
 
     func isLinked() -> Bool {
-        return DBAccountManager.sharedManager().linkedAccount != .None
+        return DBSession.sharedSession().isLinked()
     }
 
     func unLink() {
-        DBAccountManager.sharedManager().linkedAccount?.unlink()
-    }
-
-    func saveFile(filename: String, data: NSData) -> Result<(), NSError> {
-        let path = DBPath.root().childPath(filename)
-        return DBFilesystem.sharedFilesystem().createFile(path) >>- { $0.writeData(data) }
-    }
-
-    func getFiles() -> Result<[String], NSError> {
-        let result = DBFilesystem.sharedFilesystem().listFolder(DBPath.root())
-        let filePaths: [DBFileInfo] -> [String] = { $0.map { $0.path.stringValue() } }
-        return filePaths <^> result
-    }
-
-    func getFile(filename: String) -> Result<NSData, NSError> {
-        let path = DBPath.root().childPath(filename)
-        return DBFilesystem.sharedFilesystem().openFile(path) >>- { file in
-            let data = file.readData()
-            file.close()
-            return data
-        }
-    }
-
-    private func setupFilesystem(account: DBAccount) {
-        DBFilesystem.setSharedFilesystem <^> DBFilesystem(account: account)
-        let filesystem = DBFilesystem.sharedFilesystem()
-
-        watchForFileChanges()
-        filesystem.addObserver(self) {
-            if filesystem.completedFirstSync && !filesystem.status.metadata.inProgress {
-                var token: dispatch_once_t = 0
-                dispatch_once(&token, self.completePreload)
-            }
-        }
-    }
-
-    private func completePreload() {
-        let filesystem = DBFilesystem.sharedFilesystem()
-        filesystem.removeObserver(self)
-        NSNotificationCenter.defaultCenter().postNotificationName(InitialSyncCompletedNotificationName, object: .None)
-        watchForFileChanges()
-    }
-
-    private func watchForFileChanges() {
-        DBFilesystem.sharedFilesystem().addObserver(self, forPathAndDescendants: DBPath.root()) {
-            _ = self.observer?.serviceDidUpdate()
-        }
+        DBSession.sharedSession().unlinkAll()
     }
 }
--- Poppins/ViewControllers/LinkAccountViewController.swift
+++ Poppins/ViewControllers/LinkAccountViewController.swift
@@ -1,33 +1,6 @@
 class LinkAccountViewController: UIViewController {
-    @IBOutlet weak var syncingView: UIView!
-    @IBOutlet weak var syncingViewOffsetConstraint: NSLayoutConstraint!
-    @IBOutlet weak var linkDropboxButton: Button!
-
     var controller: LinkAccountController?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "authSuccessful", name: AccountLinkedNotificationName, object: .None)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "initialSyncCompleted", name: InitialSyncCompletedNotificationName, object: .None)
-    }
-
-    deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
-    }
-
-    func authSuccessful() {
-        dispatch_async(dispatch_get_main_queue()) {
-            UIView.animateWithDuration(1.0) {
-                self.syncingView.alpha = 1.0
-            }
-        }
-    }
-
-    func initialSyncCompleted() {
-        NSNotificationCenter.defaultCenter().postNotificationName("LinkingComplete", object: .None)
-    }
-    
     @IBAction func linkDropbox() {
         controller?.linkAccount(self)
     }
