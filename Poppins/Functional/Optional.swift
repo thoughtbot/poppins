723742c72d7cb299c93df418a2bd2b7734f1925f
Tony DiPasquale <tony@thoughtbot.com>
2015-03-20 11:26:46 -0400

    Add Runes

--- Poppins/Functional/Optional.swift
+++ Poppins/Functional/Optional.swift
@@ -1,32 +1,6 @@
 import LlamaKit
 
-func >>-<T, U>(a: T?, f: T -> U?) -> U? {
-    return a.flatMap(f)
-}
-
-func <^><T, U>(f: T -> U, a: T?) -> U? {
-    return a.map(f)
-}
-
-func <*><T, U>(f: (T -> U)?, a: T?) -> U? {
-    return a.apply(f)
-}
-
 extension Optional {
-    func flatMap<U>(f: T -> U?) -> U? {
-        switch self {
-        case let .Some(x): return f(x)
-        case .None: return .None
-        }
-    }
-
-    func apply<U>(f: (T -> U)?) -> U? {
-        switch f {
-        case let .Some(fx): return self.map(fx)
-        default: return .None
-        }
-    }
-
     func toResult() -> Result<T, NSError> {
         switch self {
         case let .Some(x): return success(x)
