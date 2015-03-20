345da47be1325c3574922c90fbcac279115a44cd
Tony DiPasquale <tony@thoughtbot.com>
2015-03-20 17:38:46 -0400

    Remove Hockey and makeing archive work
    It's not a part of this git commit but, Cascade has a Frameworks folder
    in it's .framework directory that is automatically generated I think.
    Anyways, it cannot be there to post to the store. We should figure out
    how to solve this long term but for now, make sure to delete that folder
    before Archiving a build.

--- Poppins/Models/HockeyManager.swift
+++
@@ -1,8 +0,0 @@
-struct HockeyManager {
-    static func configure() {
-        let manager = BITHockeyManager.sharedHockeyManager()
-        manager.configureWithIdentifier("fe9cf520171adf0c5010922e57bc2a83")
-        manager.startManager()
-        manager.authenticator.authenticateInstallation()
-    }
-}
