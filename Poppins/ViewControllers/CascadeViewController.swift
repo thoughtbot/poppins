a57e644dac41229ddad48815b2945aae0f498c23
Tony DiPasquale <tony@thoughtbot.com>
2015-03-20 16:48:35 -0400

    Add gif Preview view on hold
    When long pressing a collection view cell, a view pops open to display a
    preview of the touched gif. It will dissapear when the hold is ended.

--- Poppins/ViewControllers/CascadeViewController.swift
+++ Poppins/ViewControllers/CascadeViewController.swift
@@ -6,12 +6,17 @@ import Runes
 
 class CascadeViewController: UICollectionViewController, CascadeLayoutDelegate {
     var controller: CascadeController?
+    var holdGestureRecognizer: UILongPressGestureRecognizer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let layout = collectionView?.collectionViewLayout as? CascadeLayout
         layout?.delegate = self
         controller?.syncWithTHECLOUD()
+
+        holdGestureRecognizer = UILongPressGestureRecognizer(target: self, action: "hold:")
+        holdGestureRecognizer?.minimumPressDuration = 0.5
+        holdGestureRecognizer >>- { self.collectionView?.addGestureRecognizer($0) }
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -54,6 +59,25 @@ class CascadeViewController: UICollectionViewController, CascadeLayoutDelegate {
         if let source = gifItemSource {
             let activityVC = UIActivityViewController(activityItems: [source], applicationActivities: [])
             presentViewController(activityVC, animated: true, completion: .None)
+        }
+    }
+
+    var preview: PreviewView?
+
+    func hold(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == .Began {
+            let point = gesture.locationInView(collectionView)
+            let indexPath = collectionView?.indexPathForItemAtPoint(point)
+            let path = indexPath >>- { self.controller?.viewModel.imagePathForIndexPath($0) }
+            let size = indexPath >>- { self.controller?.viewModel.imageSizeForIndexPath($0) }
+
+            preview = PreviewView.create()
+            preview?.controller = path >>- { p in size.map { PreviewController(path: p, size: $0) } }
+            preview?.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+            preview.map { self.view.addSubview($0) }
+        } else if gesture.state == .Ended {
+            preview?.removeFromSuperview()
+            preview = .None
         }
     }
 }
