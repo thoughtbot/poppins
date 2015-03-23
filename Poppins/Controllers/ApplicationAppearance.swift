61e8eb4c45f6e5c1d73d6bc77745368609347c8a
Tony DiPasquale <tony@thoughtbot.com>
2015-03-23 19:51:09 -0400

    Set navigation title and bar button items to white

--- Poppins/Controllers/ApplicationAppearance.swift
+++ Poppins/Controllers/ApplicationAppearance.swift
@@ -1,5 +1,11 @@
+import UIKit
+
 struct ApplicationAppearance {
     static func setupAppearance() {
         UINavigationBar.appearance().setBackgroundImage(UIImage(named: "NavigationBarBackground"), forBarMetrics: .Default)
+
+        let attributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributes, forState: .Normal)
+        UINavigationBar.appearance().titleTextAttributes = attributes
     }
 }
