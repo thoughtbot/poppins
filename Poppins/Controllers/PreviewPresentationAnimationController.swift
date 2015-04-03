8aa7cee0891308a31fa0191839ee7e566ab799c0
Tony DiPasquale <tony@thoughtbot.com>
2015-04-03 18:28:40 -0400

    Use custom transition to preview GIFs
    This uses a custom view controller transition to animate open the
    preview of a GIF.

---
+++ Poppins/Controllers/PreviewPresentationAnimationController.swift
@@ -0,0 +1,62 @@
+import Foundation
+import UIKit
+import Runes
+
+class PreviewPresentationAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+    let isPresenting: Bool
+    let startingFrame: CGRect
+    let duration: NSTimeInterval
+
+    init(isPresenting: Bool, startingFrame: CGRect, duration: NSTimeInterval = 0.3) {
+        self.isPresenting = isPresenting
+        self.startingFrame = startingFrame
+        self.duration = duration
+        super.init()
+    }
+
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return duration
+    }
+
+    var animationOptions: UIViewAnimationOptions {
+        return UIViewAnimationOptions.AllowAnimatedContent | UIViewAnimationOptions.LayoutSubviews
+    }
+
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        if isPresenting {
+            animatePresentationWithTransitionContext(transitionContext)
+        } else {
+            animateDismissalWithTransitionContext(transitionContext)
+        }
+    }
+
+    func animatePresentationWithTransitionContext(transitionContext: UIViewControllerContextTransitioning) {
+        let presentedController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey) as? PreviewViewController
+        let presentedControllerView = transitionContext.viewForKey(UITransitionContextToViewKey)
+        let containerView = transitionContext.containerView()
+
+        let finalFrame = presentedController.map { transitionContext.finalFrameForViewController($0) }
+        presentedControllerView?.frame = startingFrame
+        presentedController?.gifView.frame = CGRect(origin: CGPointZero, size: startingFrame.size)
+        presentedControllerView?.alpha = 0
+        presentedControllerView.map(containerView.addSubview)
+
+        UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0, options: animationOptions, animations: {
+            presentedControllerView?.alpha = 1
+            presentedControllerView?.frame = finalFrame ?? CGRectZero
+        }) { completed in
+            transitionContext.completeTransition(completed)
+        }
+    }
+
+    func animateDismissalWithTransitionContext(transitionContext: UIViewControllerContextTransitioning) {
+        let presentedControllerView = transitionContext.viewForKey(UITransitionContextFromViewKey)
+
+        UIView.animateWithDuration(transitionDuration(transitionContext), delay: 0, options: animationOptions, animations: {
+            presentedControllerView?.alpha = 0
+            presentedControllerView?.frame = self.startingFrame
+        }) { completed in
+            transitionContext.completeTransition(completed)
+        }
+    }
+}
