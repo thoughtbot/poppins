0dae8a6c89acf8dfa10712a5841a414c66afff44
Tony DiPasquale <tony@thoughtbot.com>
2015-04-03 18:28:40 -0400

    Use custom transition to preview GIFs
    This uses a custom view controller transition to animate open the
    preview of a GIF.

---
+++ Poppins/Controllers/PreviewPresentationController.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+private let PreviewGIFPadding: CGFloat = 20
+
+class PreviewPresentationController: UIPresentationController {
+    let dimmingView = UIView()
+    let frame: CGRect
+
+    init(presentedViewController: UIViewController!, presentingViewController: UIViewController!, frame: CGRect) {
+        assert(frame.height != 0)
+        self.frame = frame
+        super.init(presentedViewController: presentedViewController, presentingViewController: presentingViewController)
+    }
+
+    override func presentationTransitionWillBegin() {
+        dimmingView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        dimmingView.frame = containerView.bounds
+        dimmingView.alpha = 0
+
+        containerView.addSubview(dimmingView)
+        containerView.addSubview(presentedView())
+
+        let transitionCoordinator = presentingViewController.transitionCoordinator()
+        transitionCoordinator?.animateAlongsideTransition({ _ in
+            self.dimmingView.alpha = 1
+        }, completion: .None)
+    }
+
+    override func presentationTransitionDidEnd(completed: Bool) {
+        if !completed {
+            dimmingView.removeFromSuperview()
+        }
+    }
+
+    override func dismissalTransitionWillBegin() {
+        let transitionCoordinator = presentingViewController.transitionCoordinator()
+        transitionCoordinator?.animateAlongsideTransition({ _ in
+            self.dimmingView.alpha = 0
+        }, completion: .None)
+    }
+
+    override func dismissalTransitionDidEnd(completed: Bool) {
+        if completed {
+            dimmingView.removeFromSuperview()
+        }
+    }
+
+    override func frameOfPresentedViewInContainerView() -> CGRect {
+        let bounds = containerView.bounds
+        let fullSize = CGSize(width: bounds.width - PreviewGIFPadding, height: bounds.height - PreviewGIFPadding)
+        let aspectRatio = frame.width / frame.height
+
+        let aspectHeight = fullSize.width / aspectRatio
+        let aspectWidth = fullSize.height * aspectRatio
+
+        let size: CGSize
+
+        if aspectHeight > fullSize.height {
+            size = CGSize(width: aspectWidth, height: fullSize.height)
+        } else {
+            size = CGSize(width: fullSize.width, height: aspectHeight)
+        }
+
+        let point = CGPoint(x: round((bounds.width - size.width) / 2), y: round((bounds.height - size.height) / 2))
+        return CGRect(origin: point, size: size)
+    }
+}
