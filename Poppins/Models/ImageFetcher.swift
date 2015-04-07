f60cddca1406d642e0a655cb566a267dc5edfe65
Tony DiPasquale <tony@thoughtbot.com>
2015-04-07 19:23:24 -0400

    Use NSOperation subclass for faster image fetching
    Bringing back the NSOperation subclass to have more concurrent requests
    fetching images from the disk. This improves the loading performancs and
    helps with some memory warning issues and crashing seen with the simple
    dispatch only method.

--- Poppins/Models/ImageFetcher.swift
+++ Poppins/Models/ImageFetcher.swift
@@ -4,33 +4,44 @@ import Runes
 class ImageFetcher {
     let imageCache: Cache<UIImage>
     let purger: CachePurger
+    let operationQueue: AsyncQueue
     let lockQueue = dispatch_queue_create("com.thoughtbot.pop-n-lock", nil)
     var inProgress: [String] = []
 
     init() {
         imageCache = Cache<UIImage>()
         purger = CachePurger(cache: imageCache)
+        operationQueue = AsyncQueue(name: "PoppinsCacheQueue", maxOperations: 10)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("didReceiveMemoryWarning"), name: UIApplicationDidReceiveMemoryWarningNotification, object: .None)
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    func didReceiveMemoryWarning() {
+        inProgress = []
     }
 
     func fetchImage(size: CGSize, path: String) -> UIImage? {
-        if size == CGSizeZero { return .None }
-
         if let image = imageCache.itemForKey(path) {
             return image
         } else {
+            if size == CGSizeZero { return .None }
             dispatch_async(lockQueue) {
                 if contains(self.inProgress, path) { return }
 
-                let data = NSData(contentsOfFile: path)
-                let image = data >>- imageForData
-                let scaledImage = image?.imageForSize(size)
-
-                curry(self.imageCache.setItem) <^> scaledImage <*> path
-
-                self.inProgress.removeAtIndex <^> find(self.inProgress, path)
-                NSNotificationCenter.defaultCenter().postNotificationName("CacheDidUpdate", object: .None)
+                let operation = ImageFetcherOperation(path: path, size: size) { image in
+                    dispatch_async(self.lockQueue) {
+                        curry(self.imageCache.setItem) <^> image <*> path
+                        self.inProgress.removeAtIndex <^> find(self.inProgress, path)
+                        NSNotificationCenter.defaultCenter().postNotificationName("CacheDidUpdate", object: .None)
+                    }
+                }
+                self.operationQueue.addOperation(operation)
             }
             return .None
         }
     }
 }
+
