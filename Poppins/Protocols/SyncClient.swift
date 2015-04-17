06503dd5d6f85bcb32339c020fabc664b25d0400
Tony DiPasquale <tony@thoughtbot.com>
2015-04-16 21:20:53 -0400

    Fetch the share url as part of the sync

--- Poppins/Protocols/SyncClient.swift
+++ Poppins/Protocols/SyncClient.swift
@@ -1,4 +1,5 @@
 protocol SyncClient {
     func getFiles() -> Signal<[FileInfo]>
     func getFile(path: String, destinationPath: String) -> Signal<String>
+    func getShareURL(path: String) -> Signal<String>
 }
