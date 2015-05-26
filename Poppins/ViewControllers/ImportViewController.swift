657d6e292ebb4e894333a84cea22a1cd7b16363a
Tony DiPasquale <tony@thoughtbot.com>
2015-05-26 17:48:35 -0400

    Open import view when new image on pasteboard

--- Poppins/ViewControllers/ImportViewController.swift
+++ Poppins/ViewControllers/ImportViewController.swift
@@ -28,6 +28,8 @@ class ImportViewController: UIViewController {
         if let viewModel = controller?.viewModel {
             imageView.animateWithImageData(data: viewModel.imageData)
             imageView.startAnimatingGIF()
+            setImageType(viewModel.imageType)
+
         }
 
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: .None)
@@ -41,8 +43,8 @@ class ImportViewController: UIViewController {
         if let size = notification.userInfo?[UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size,
            let duration = notification.userInfo?[UIKeyboardAnimationDurationUserInfoKey]?.doubleValue
         {
-            let yTotal = CGRectGetMidY(view.frame) * 2
-            let yOffset = CGRectGetMaxY(view.frame) - ((yTotal - size.height) - 20)
+            let yTotal = view.superview?.frame.height ?? 0
+            let yOffset = CGRectGetMaxY(view.frame) - (yTotal - (size.height + 20))
             let newFrame = CGRect(origin: CGPoint(x: view.frame.origin.x, y: view.frame.origin.y - yOffset), size: view.frame.size)
 
             UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseInOut, animations: {
@@ -52,8 +54,12 @@ class ImportViewController: UIViewController {
     }
 
     @IBAction func save() {
-        // add image to core data
-        // upload image to dropbox
+        if let image = imageView.image {
+            let row = extensionPicker.selectedRowInComponent(ImportViewPickerDefaultComponent)
+            let ext = pickerView(extensionPicker, titleForRow: row, forComponent: ImportViewPickerDefaultComponent)
+            controller?.saveAndUploadImage(image, name: "\(imageNameField.text)\(ext)")
+            dismissViewControllerAnimated(true, completion: .None)
+        }
     }
 
     @IBAction func cancel() {
@@ -61,6 +67,7 @@ class ImportViewController: UIViewController {
     }
 }
 
+private let ImportViewPickerDefaultComponent = 0
 extension ImportViewController: UIPickerViewDataSource {
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
         return 1
@@ -79,6 +86,18 @@ extension ImportViewController: UIPickerViewDelegate {
         case 2: return ".jpeg"
         default: return ""
         }
+    }
+}
+
+extension ImportViewController {
+    func setImageType(type: String) {
+        let row: Int
+        switch type {
+        case JPEGType: row = 1
+        case PNGType: row = 2
+        default: row = 0
+        }
+        extensionPicker.selectRow(row, inComponent: ImportViewPickerDefaultComponent, animated: true)
     }
 }
 
