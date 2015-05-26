48ae4447516f67a0fece99855326a05aa2badb0f
Tony DiPasquale <tony@thoughtbot.com>
2015-05-26 17:47:57 -0400

    Use image type from pasteboard to select extension

--- Poppins/Controllers/ImportController.swift
+++ Poppins/Controllers/ImportController.swift
@@ -1,7 +1,15 @@
 struct ImportController {
     let imageData: NSData
+    let imageType: String
+    let store: ImageStore
+    let client: SyncClient
 
     var viewModel: ImportViewModel {
-        return ImportViewModel(imageData: imageData)
+        return ImportViewModel(imageData: imageData, imageType: imageType)
+    }
+
+    func saveAndUploadImage(image: UIImage, name: String) {
+        let path = store.saveImageData(imageData, name: name, aspectRatio: Double(image.aspectRatio))
+        path.map { client.uploadFile(name, localPath: $0) }
     }
 }
